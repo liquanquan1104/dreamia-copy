51b034c5791df91fea6dde23e64154dae4b312e4
liquanquan1104 <liquanquan1104@gmail.com>
2026-04-24 17:41:54 +0800

    feat: upload组件Tooltip及semi UPload

--- src/layout/components/prompt-input/components/resource-upload/empty-card/index.tsx
+++ src/layout/components/prompt-input/components/resource-upload/empty-card/index.tsx
@@ -1,16 +1,19 @@
 import style from './index.module.less'
 import { IconPlusStroked } from '@douyinfe/semi-icons';
+import { Tooltip } from '@douyinfe/semi-ui';
 
 interface EmptyCardProps {
   icon?: React.ReactNode;
   text?: string;
+  toolTip?:string;
 }
 
-export default function EmptyCard({icon = <IconPlusStroked style={{fontSize: 16}} />, text}: EmptyCardProps) {
-  return (
+export default function EmptyCard({icon = <IconPlusStroked style={{fontSize: 16}} />, text, toolTip}: EmptyCardProps) {
+  const content = (
     <div className={style['card']}>
-      {icon}
-      <div className={style['card-text']}>{text}</div>
+       {icon}
+       <div className={style['card-text']}>{text}</div>
     </div>
-  );
+  )
+  return toolTip ? <Tooltip content={toolTip} className={style['card-tooltip']} showArrow={false}>{content}</Tooltip> : content;
 }
--- src/layout/components/prompt-input/prompt-input-area/index.tsx
+++ src/layout/components/prompt-input/prompt-input-area/index.tsx
@@ -2,16 +2,23 @@
  import EmptyCard from '../components/resource-upload/empty-card'
  import { IconSonicStroked } from '@douyinfe/semi-icons';
  import ResourceCard from '../components/resource-upload/resource-card'
+ import {Upload} from '@douyinfe/semi-ui';
  
  export default function PromptInputArea() {
   return (
     <div className={style['prompt-input-area']}>
       <div className={style['prompt-input-resource']}>
-        <EmptyCard  text='角色' icon={<IconSonicStroked style={{fontSize: 16}} />} />
+        
+        <ResourceCard />
       </div>
       <div className={style['prompt-input-content']}>
         Tiptap输入框
-        <ResourceCard />
+       <Upload action="https://api.semi.design/upload">
+          <EmptyCard  
+            toolTip='角色'
+            text='角色' 
+            icon={<IconSonicStroked style={{fontSize: 16}} />} />
+       </Upload>
       </div>
     </div>
   )
