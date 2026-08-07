f3b850af6702bc0cc36c4fd55597732926af23d0
lihuan.010 <lihuan.010@bytedance.com>
2026-08-07 10:13:39 +0800

    feat: 主体引用，创建主ti

--- src/layout/components/prompt-input/input-tool-bar/components/cite-subject-pannel/index.tsx
+++ src/layout/components/prompt-input/input-tool-bar/components/cite-subject-pannel/index.tsx
@@ -1,7 +1,8 @@
 import styles from './index.module.less';
 import EmptyCiteSubject from '@/assets/subject-empty.svg';
+import { Dropdown } from '@douyinfe/semi-ui'
 import BaseButton from '@/components/base-button';
-import { IconPlusStroked } from '@douyinfe/semi-icons';
+import { IconPlusStroked, IconCloudUploadStroked, IconImageStroked } from '@douyinfe/semi-icons';
 
 interface CiteSubjectPannelProps {
     title?: string;
@@ -11,17 +12,31 @@ interface CiteSubjectPannelProps {
 const baseCls = 'cite-subject-pannel';
 
 export default function CiteSubjectPannel({ title, subject }: CiteSubjectPannelProps) {
+    const renderDropdownItems = () => {
+        return (
+                <Dropdown.Menu style={{ width: 160 }}>
+                        <Dropdown.Item icon={<IconCloudUploadStroked />} className={styles['dropdown-item']}>从本地添加</Dropdown.Item>
+                        <Dropdown.Item icon={<IconImageStroked />} className={styles['dropdown-item']}>从资产添加</Dropdown.Item>
+                    </Dropdown.Menu>
+        )
+    }
     const renderEmpty = () => {
         return (
             <div className={styles[`${baseCls}-subject-empty`]}>
                 <img src={EmptyCiteSubject} alt="暂无引用内容" />
                 <div className={styles[`${baseCls}-subject-empty-desc`]}>你还没有创建过主体</div>
-                <BaseButton text='创建主体' icon= {<IconPlusStroked />} />
+                <Dropdown  
+                    trigger='click'
+                    position='top'
+                    render={renderDropdownItems()}
+                >
+                    <BaseButton text='创建主体' icon= {<IconPlusStroked  style={{ fontSize: 15 }} />}/>
+                </Dropdown>
             </div>
         )
     }
     return (
-        <div className={baseCls}>
+        <div className={styles[baseCls]}>
             <div className={styles[`${baseCls}-title`]}>{title}</div>
             {!subject ? 
                 renderEmpty() :    
